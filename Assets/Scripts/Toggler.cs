d303cd2756dd21992b8c9ce5fd52e02c50ac8158
Hatemsla <kalashnikovjan@yandex.ru>
2021-04-14 20:34:25 +0300

    Алерт
    Сделал алерт. Плохо работает с двумя и более тогглерами):

--- Assets/Scripts/Toggler.cs
+++ Assets/Scripts/Toggler.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Toggler : MonoBehaviour
 {
     public bool value;
+    public GameObject DeviceAlert;
     [SerializeField] private Animator animator;
-
     private static readonly int Value = Animator.StringToHash(name: "Value");
     private void Awake()
     {
         if (this.animator == null) this.animator = GetComponent<Animator>();
 
         this.animator.SetBool(id: Value, this.value);
+        
+    }
+
+    public void isClick()
+    {
+        DeviceAlert.SetActive(true);
+    }
+
+    public void isChangedToggler()
+    {
+        Togler();
     }
 
     public void Togler()
     {
         this.value = !this.value;
-
         this.animator.SetBool(id: Value, this.value);
     }
 }
